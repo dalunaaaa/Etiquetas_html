5c43508fecbc73be36455cad8dfb5b53d2d2742b
danialuna <dmluna@scl.edu.gt>
2024-05-30 11:29:01 -0600

    header y footer

--- header/header.js
+++ header/header.js
@@ -1,2 +1,4 @@
 let header_DOM = document.querySelector(".header")
- header_DOM.innerHTML = `hola soy header`;
+ header_DOM.innerHTML = `<h1>Gramatica HTML</h1>
+ <a href="https://github.com/dalunaaaa/Etiquetas_html" class="git-button">Git</a>
+`;
