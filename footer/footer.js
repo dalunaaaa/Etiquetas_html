f97aed67707533d392c0f989c1725ee94043f5b7
danialuna <dmluna@scl.edu.gt>
2024-05-30 17:26:06 -0600

    agregue los links al footer

--- footer/footer.js
+++ footer/footer.js
@@ -2,10 +2,10 @@ let footer_DOM = document.querySelector(".footer")
 footer_DOM.innerHTML = `
 
 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq-IciWxLW5lsM2Oco4VluWyfOBSmgukVu6w&s" alt="Logo" class="footer-logo">
-        <div class="footer-buttons">
-            <button>@dalunaaaa</button>
-            <button>@adrodas</button>
-            <button>@ROCKEMMA</button>
-            <button>@slopez</button>
-        </div>
+<div class="footer-buttons">
+    <a href="https://github.com/dalunaaaa?tab=repositories" class="footer-button">@dalunaaaa</a>
+    <a href="https://github.com/xalyrodas?tab=repositories" class="footer-button">@adrodas</a>
+    <a href="https://github.com/ROCKEMMA?tab=repositories" class="footer-button">@ROCKEMMA</a>
+    <a href="https://github.com/SantiagoEmilio" class="footer-button">@slopez</a>
+</div>
 `;
