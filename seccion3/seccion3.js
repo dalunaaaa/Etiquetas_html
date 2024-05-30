be00ffc8d47d74369db243b3334c3286d66da01c
adrodas <adrodas@scl.edu.gt>
2024-05-30 15:49:40 -0600

    seccion3_terminada

--- seccion3/seccion3.js
+++ seccion3/seccion3.js
@@ -1,2 +1,49 @@
 let seccion3_DOM = document.querySelector(".seccion3")
-seccion3_DOM.innerHTML = `hola soy seccion3`;
+seccion3_DOM.innerHTML = `
+<div class="seccion3">
+        <h1>Bloques</h1>
+        <table>
+            <tr>
+                <th>Etiqueta</th>
+                <th>Descripción</th>
+            </tr>
+            <tr>
+                <td>&lt;form&gt;</td>
+                <td>Define un formulario HTML</td>
+            </tr>
+            <tr>
+                <td>&lt;input&gt;</td>
+                <td>Define un control de entrada de datos</td>
+            </tr>
+            <tr>
+                <td>&lt;label&gt;</td>
+                <td>Define una etiqueta para un control de formulario</td>
+            </tr>
+            <tr>
+                <td>&lt;textarea&gt;</td>
+                <td>Define un campo de texto multilínea</td>
+            </tr>
+            <tr>
+                <td>&lt;button&gt;</td>
+                <td>Define un botón clickable</td>
+            </tr>
+            <tr>
+                <td>&lt;select&gt;</td>
+                <td>Define un menú desplegable</td>
+            </tr>
+            <tr>
+                <td>&lt;option&gt;</td>
+                <td>Define una opción en un menú desplegable</td>
+            </tr>
+            <tr>
+                <td>&lt;fieldset&gt;</td>
+                <td>Agrupa controles relacionados en un formulario</td>
+            </tr>
+            <tr>
+                <td>&lt;legend&gt;</td>
+                <td>Define un título para el contenido de un &lt;fieldset&gt;</td>
+            </tr>
+        </table>
+    </div>
+
+`;
